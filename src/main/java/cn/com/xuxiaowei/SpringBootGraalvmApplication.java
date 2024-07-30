b36bc6ff0e488f63f23d01edd36052598ed26537
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2024-07-31 07:58:16 +0800

    init project

---
+++ src/main/java/cn/com/xuxiaowei/SpringBootGraalvmApplication.java
@@ -0,0 +1,13 @@
+package cn.com.xuxiaowei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGraalvmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGraalvmApplication.class, args);
+	}
+
+}
